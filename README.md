9a058776319d59cf5c33ab76b9b80a09188aa0a1
Greg Kops <greg@thinktopography.com>
2017-04-25 21:52:29 -0400

    readme, query, and body

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # redux-api-request
+
+<a href="https://circleci.com/gh/thinktopography/redux-api-request">
+  <img src="https://img.shields.io/circleci/project/thinktopography/redux-api-request.svg?maxAge=600" alt="Build Status" >
+</a>
+<a href="https://codeclimate.com/github/thinktopography/redux-api-request">
+  <img src="https://img.shields.io/codeclimate/github/thinktopography/redux-api-request.svg?maxAge=600" alt="Code Climate" />
+</a>
+<a href="https://codeclimate.com/github/thinktopography/redux-api-request/coverage">
+  <img src="https://img.shields.io/codeclimate/coverage/github/thinktopography/redux-api-request.svg?maxAge=600" alt="Code Coverage" />
+</a>
+
 Redux middleware for making api requests
+
+## Installation
+Install with [npm](http://npmjs.com) or [yarn](https://yarnpkg.com):
+
+```sh
+npm install --save redux-api-request
+```
+
+## Usage
+Using redux-api-request in your application is easy:
+
+```javascript
+# action creator
+export const signin = (email, password) => ({
+  type: 'API_REQUEST',
+  method: 'POST',
+  endpoint: '/admin/signin',
+  params: { email, password },
+  request: actionTypes.SIGNIN_REQUEST,
+  success: actionTypes.SIGNIN_SUCCESS,
+  failure: actionTypes.SIGNIN_FAILURE
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,71 @@
-import middleware from './middleware'
-import * as action_types from './action_types'
+import _ from 'lodash'
+import qs from 'qs'
+import * as actionTypes from './action_types'
 
-export {
-  action_types
+export default rest => store => next => action => {
+
+  const [string, namespace, type] = action.type.match(/([\a-z0-9_\.]*)?\/?([A-Z0-9_]*)/)
+
+  if(type !== actionTypes.API_REQUEST) {
+    return next(action)
+  }
+
+  const headers = {
+    'Content-Type': 'application/json',
+    ...action.headers ? action.headers : {}
+  }
+
+  const method = action.method
+
+  const path = (action.query && method === 'GET') ? `${action.endpoint}?${qs.stringify(action.query)}` : action.endpoint
+
+  const entity = (action.body && method !== 'GET') ? action.body : null
+
+  const request = _.omitBy({ headers, method, path, entity }, _.isNil)
+
+  coerceArray(action.request).map(requestAction => {
+    store.dispatch({
+      type: withNamespace(namespace, requestAction),
+      cid: action.cid,
+      request
+    })
+  })
+
+
+  const success = (json) => {
+
+    coerceArray(action.success).map(successAction => {
+      store.dispatch({
+        type: withNamespace(namespace, successAction),
+        cid: action.cid,
+        ...action.meta,
+        result: json
+      })
+    })
+
+  }
+
+  const failure = (response) => {
+
+    coerceArray(action.failure).map(failureAction => {
+      store.dispatch({
+        type: withNamespace(namespace, failureAction),
+        cid: action.cid,
+        ...action.meta,
+        result: response.entity
+      })
+    })
+
+  }
+
+  return rest({ headers, method, path, entity }).then(response => response.entity).then(success, failure)
+
 }
 
-export default middleware
+const coerceArray = (value) => {
+  return value ? (!_.isArray(value) ? [value] : value) : []
+}
+
+const withNamespace = (namespace, type) => {
+  return namespace ? `${namespace}/${type}` : type
+}
